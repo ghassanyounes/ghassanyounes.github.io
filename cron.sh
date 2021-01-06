221eb3849f45ab3a707723a0f815a71c019af87e
Ghassan Younes <ghassan@ghassanyounes.com>
2021-01-06 01:42:58 -0800

    website

---
+++ cron.sh
@@ -0,0 +1,4 @@
+git --git-dir=/home/u263441050/public_html/public_html/BTPBF/.git --work-tree=/home/u263441050/public_html/public_html/BTPBF/ fetch && git --git-dir=/home/u263441050/public_html/public_html/BTPBF/.git --work-tree=/home/u263441050/public_html/public_html/BTPBF/ pull
+echo "yeet"
+git --git-dir=/home/u263441050/public_html/public_html/BTPBF/.git --work-tree=/home/u263441050/public_html/public_html/BTPBF/ checkout -- '*'
+
