2c838e463fe558a3929a0cc14aa114431b1d1309
Ghassan Younes <ghassan@ghassanyounes.com>
2021-01-06 02:19:48 -0800

    fix cron shell file

--- cron.sh
+++
@@ -1,4 +0,0 @@
-git --git-dir=/home/u263441050/public_html/public_html/BTPBF/.git --work-tree=/home/u263441050/public_html/public_html/BTPBF/ fetch && git --git-dir=/home/u263441050/public_html/public_html/BTPBF/.git --work-tree=/home/u263441050/public_html/public_html/BTPBF/ pull
-echo "yeet"
-git --git-dir=/home/u263441050/public_html/public_html/BTPBF/.git --work-tree=/home/u263441050/public_html/public_html/BTPBF/ checkout -- '*'
-
