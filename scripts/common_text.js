51820ff586ff3c9d13096f2a65a1235bf1bed5e5
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-01-24 03:20:59 -0800

    New pages, secret rainbow, and responsive layout for mobile :)

--- scripts/common_text.js
+++ scripts/common_text.js
@@ -1,5 +1,40 @@
 const d = new Date();
+document.getElementById("footer").innerHTML = `<a target=blank href="http://github.com/ghassanyounes" id=github ><i class="fab fa-github fa-1x"></i></a>
+<a target=blank href="http://instagram.com/the__ghassan" id=instagram ><i class="fab fa-instagram fa-1x"></i></a>\n
+<a target=blank id=linkedin href="http://linkedin.com/in/ghassanyounes" target=blank><i class="fab fa-linkedin fa-1x"></i></a>\n
+<a target=blank href="http://twitter.com/the__ghassan" id=twitter><i class="fab fa-twitter fa-1x"></i></a> \n
+<a target=blank href="mailto:ghassan@ghassanyounes.com" id=email><i class="fas fa-at fa-1x"></i></a>\n
+<p id="copyright"></p>`;
 document.getElementById("copyright").innerHTML = `&copy; Ghassan Younes ${d.getFullYear()}`;
+
+if (document.getElementById("home") != null)
+  document.getElementById("home").innerHTML = `Home`;
+  if (document.getElementById("shophome") != null)
+    document.getElementById("shophome").innerHTML = `Shop Main Page`;
+if (document.getElementById("contact") != null)
+  document.getElementById("contact").innerHTML = `Resumé &amp; Contact`;
+if (document.getElementById("projs") != null)
+  document.getElementById("projs").innerHTML = `Professional Life &amp; Work`;
+if (document.getElementById("blog") != null)
+  document.getElementById("blog").innerHTML = `Food Blog`;
+if (document.getElementById("store") != null)
+  document.getElementById("store").innerHTML = `Store`;
+if (document.getElementById("about") != null)
+  document.getElementById("about").innerHTML = `About Me`;
+if (document.getElementById("order") != null)
+  document.getElementById("order").innerHTML = `Order Here!`;
+if (document.getElementById("highlights") != null)
+  document.getElementById("highlights").innerHTML = `Highlights`;
+if (document.getElementById("reviews") != null)
+  document.getElementById("reviews").innerHTML = `Reviews`;
+if (document.getElementById("bloghome") != null)
+  document.getElementById("bloghome").innerHTML = `Blog Main Page`;
+if (document.getElementById("newestpost") != null)
+  document.getElementById("newestpost").innerHTML = `Latest Posts`;
+if (document.getElementById("highlights") != null)
+  document.getElementById("highlights").innerHTML = `Highlights`;
+if (document.getElementById("recipes") != null)
+  document.getElementById("recipes").innerHTML = `Recipes`;
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
--- scripts/name-change-color.js
+++ scripts/name-change-color.js
@@ -1,36 +1,45 @@
 var text = document.getElementById("name");
-var str = text.innerHTML;
+const orig = text.innerHTML;
+var running = false;
+text.onclick = function(){
+  running = ~running;
+  var str = text.innerHTML;
 
-var colours = [
-  "#d8117d", //pink
-  "#d9362d", //red
-  "#e5712a", //orange
-  "#f8be23", //yellow
-  "#069743", //green
-  "#00a3da", //blue
-  "#773e85"  //purple
-];
-colours = colours.reverse();
-var j = "";
-var k = 0;
-setInterval(function (){
-  //k++; 
-  j = "";
-  str = "Ghassan Younes";
-//no interval calls, they run all the time and cause a stack overflow
+  var colours = [
+    "#d8117d", //pink
+    "#d9362d", //red
+    "#e5712a", //orange
+    "#f8be23", //yellow
+    "#069743", //green
+    "#00a3da", //blue
+    "#773e85"  //purple
+  ];
+  colours = colours.reverse();
+  var j = "";
+  var k = 0;
+  var intervalID = setInterval(function (){
+    j = "";
+    str = orig;
+  //no interval calls, they run all the time and cause a stack overflow
 
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] != " ") {
-      k++;
-      j += `<span style="color:${
-        colours[colours.length - ((k % colours.length)) - 1]
-      };">${str[i]}</span>`;
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] != " ") {
+        k++;
+        j += `<span style="color:${
+          colours[colours.length - ((k % colours.length)) - 1]
+        };">${str[i]}</span>`;
+      }
+      if (str[i] == " ") {
+        j += `<span class="namespan"> </span>`;
+      }
     }
-    if (str[i] == " ") {
-      j += " ";
-    }
-  }
 
-  document.getElementById("name").innerHTML = j;
-  if (k == 91) { k = 0; };
-}, 1000);
+    document.getElementById("name").innerHTML = j;
+    if (k == 91) { k = 0; };
+
+    if (!running){
+      document.getElementById("name").innerHTML = orig;
+      clearInterval(intervalID);
+    }
+  }, 1000);
+}
