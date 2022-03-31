4a68babeee3599fda27a426503b56b7358b0c9b9
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-03-31 14:08:21 -0700

    fiddling with php

--- scripts/jquery_replacements.js
+++ scripts/jquery_replacements.js
@@ -4,7 +4,7 @@ $.post("scripts/listProgressDir.php", function(progressDirList) {
   //console.log(progressDirList); //"progressDirList" contains whatever listProgressDir.php returned
   let text = progressDirList.split("\n");
   for (var i = 0; i < text.length; i++) {
-    text[i] = text[i].cut(text[i].substr(20), text[i].indexOf(".html"));
+    text[i] = text[i].slice(text[i].substr(20), text[i].indexOf(".html"));
   }
   console.log(text);
   recent_prj_update = text.reduce((a,b) => (a >= b) ? a : b );
