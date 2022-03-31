03098497a9173528783203a62b8fda290085af99
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-03-31 13:39:26 -0700

    more jquery

--- scripts/jquery_replacements.js
+++ scripts/jquery_replacements.js
@@ -1,4 +1,4 @@
-$.post("listProgressDir.php", function(progressDirList) {
+$.post("./listProgressDir.php", function(progressDirList) {
   console.log(progressDirList); //"progressDirList" contains whatever listProgressDir.php returned
 });
 $('#projupdate').load('../progress_reports/20220331.html #report');
