7466095d9555bc8201a0ad6aac11028f60b62d1d
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-03-31 13:54:31 -0700

    fiddling with php

--- scripts/jquery_replacements.js
+++ scripts/jquery_replacements.js
@@ -1,4 +1,9 @@
 $.post("scripts/listProgressDir.php", function(progressDirList) {
   console.log(progressDirList); //"progressDirList" contains whatever listProgressDir.php returned
+  let text = progressDirList.split("\n");
+  text.forEach(i => {
+    i = i.substr(21);
+  });
+  console.log(text);
 });
 $('#projupdate').load('../progress_reports/20220331.html #report');
