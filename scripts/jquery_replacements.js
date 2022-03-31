91d543f41ac90ba24b85135a3a896bf47d0fdf69
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-03-31 14:11:31 -0700

    fiddling with php

--- scripts/jquery_replacements.js
+++ scripts/jquery_replacements.js
@@ -2,12 +2,11 @@ var recent_prj_update = "";
 
 $.post("scripts/listProgressDir.php", function(progressDirList) {
   let text = progressDirList.split("\n");
-  /* for (var i = 0; i < text.length; i++) {
+  for (var i = 0; i < text.length; i++) {
     text[i] = text[i].slice(0, text[i].indexOf(".html"));
-    text[i] = text[i].substr(20);
-  } */
+  } 
   console.log(text);
   recent_prj_update = text.reduce((a,b) => (a >= b) ? a : b );
   console.log(`Most recent file is ${recent_prj_update}\n`);
 });
-$('#projupdate').load('../progress_reports/20220331.html #report');
+$('#projupdate').load(`${recent_prj_update}.html #report`);
