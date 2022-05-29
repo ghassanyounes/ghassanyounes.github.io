8515a8b12882262f14fa788a4c42537621c85372
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-05-29 04:51:38 -0700

    captcha

--- scripts/contactform.php
+++ scripts/contactform.php
@@ -26,58 +26,30 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
-if(isset($_POST['submit']))
+if( empty($errors) && && isset($_POST['token_response']) )
 {
+  $recaptcha_response=$_POST['token_response'];
 
-function CheckCaptcha($userResponse) {
-        $fields_string = '';
-        $fields = array(
-            'secret' => $secret,
-            'response' => $userResponse
-        );
-        foreach($fields as $key=>$value)
-        $fields_string .= $key . '=' . $value . '&';
-        $fields_string = rtrim($fields_string, '&');
+  $request = file_get_contents($url . '?secret=' . $secret . '&response=' . $recaptcha_response);
+  $response = json_decode($request);
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
-        curl_setopt($ch, CURLOPT_POST, count($fields));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
-
-        $res = curl_exec($ch);
-        curl_close($ch);
-
-        return json_decode($res, true);
-    }
-
-    // Call the function CheckCaptcha
-    $result = CheckCaptcha($_POST['g-recaptcha-response']);
-
-    if ($result['success']) {
-        //If the user has checked the Captcha box
-        echo "Captcha verified Successfully";
-        $submission_ready = true;
-	
-    } else {
-        // If the CAPTCHA box wasn't checked
-       echo '<script>alert("Captcha failed. Please reload the page and try again");</script>';
-    }
-}
-
-if( empty($errors) && $submission_ready )
-{
-	$to = $myemail; 
-	$email_subject = "Contact form submission: $name";
-	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
-	
-	$headers = "From: form@ghassanyounes.com\n"; 
-	$headers .= "Reply-To: $email_address";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: ../contact-form-submitted.html');
+  if($response->success==true && $response->score >= 0.5){
+    $to = $myemail; 
+    $email_subject = "Contact form submission: $name";
+    $email_body = "You have received a new message. ".
+    " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+    
+    $headers = "From: form@ghassanyounes.com\n"; 
+    $headers .= "Reply-To: $email_address";
+    
+    mail($to,$email_subject,$email_body,$headers);
+    //redirect to the 'thank you' page
+    header('Location: ../contact-form-submitted.html');
+  } else {
+    echo'<script language="javascript">';
+    echo'alert("Error. Humanoid not detected.")';
+    echo'</script>';
+  }
 } 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
