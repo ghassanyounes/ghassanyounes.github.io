bddd6d032131040641aca80cd844cba9c5d0270c
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-05-29 04:30:21 -0700

    captcha

--- scripts/contactform.php
+++ scripts/contactform.php
@@ -1,6 +1,13 @@
 <?php 
+$secret_file = fopen("priv.info","r");
+$secret=fread($secret_file,filesize("priv.info"));
+fclose($secret_file);
+
 $errors = '';
 $myemail = 'ghassan@ghassanyounes.com';//<-----Put Your email address here.
+
+$submission_ready = false;
+
 if(empty($_POST['name'])  || 
    empty($_POST['email']) || 
    empty($_POST['message']))
@@ -19,14 +26,53 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
-if( empty($errors))
+if(isset($_POST['submit']))
+{
+
+function CheckCaptcha($userResponse) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => $secret,
+            'response' => $userResponse
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($res, true);
+    }
+
+    // Call the function CheckCaptcha
+    $result = CheckCaptcha($_POST['g-recaptcha-response']);
+
+    if ($result['success']) {
+        //If the user has checked the Captcha box
+        echo "Captcha verified Successfully";
+        $submission_ready = true;
+	
+    } else {
+        // If the CAPTCHA box wasn't checked
+       echo '<script>alert("Captcha failed. Please reload the page and try again");</script>';
+    }
+}
+
+if( empty($errors) && $submission_ready )
 {
 	$to = $myemail; 
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
 	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
 	
-	$headers = "From: $myemail\n"; 
+	$headers = "From: form@ghassanyounes.com\n"; 
 	$headers .= "Reply-To: $email_address";
 	
 	mail($to,$email_subject,$email_body,$headers);
